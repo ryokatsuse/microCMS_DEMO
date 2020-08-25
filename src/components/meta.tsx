3ec7e4ac198997df5c5723e0f2ae5b0f7169b493
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-08-25 23:25:29 +0900

    feat:meta.tsx修正

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -9,10 +9,6 @@ type Props = {
 const Meta: React.FC<Props> = ({ description, title }) => {
   return (
     <Head>
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta title={title ? title : `スパイス図鑑`} />
       <meta
         name="description"
